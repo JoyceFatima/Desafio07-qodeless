e294e394780beacda7caf8a9d6ccb9ddde7efc62
Joyce de Fatima <joycecosta453@hotmail.com>
2022-02-06 21:26:23 -0300

    Adicionando o componente  Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import { BrowserRouter as Routes, Route, Link } from "react-router-dom";
-
-import Page1 from "./Pages/Pages1";
-import Page2 from "./Pages/Pages2";
-import Page3 from "./Pages/Pages3";
+import { Link } from "react-router-dom";
+import Rotas from "./Routes/Rotas";
 
 function App() {
   return (
@@ -15,16 +12,15 @@ function App() {
         style={{
           display: 'flex',
           gap: '1em',
+          justifyContent: 'center',
         }}>
           <Link to="/">Home</Link>
           <Link to="users/*">Users</Link>
           <Link to="contact/*">Contact us</Link>
         </nav>
-        <Routes>
-          <Route path="/" element={<Page1 />} />
-          <Route path="users/*" element={<Page2 />} />
-          <Route path="contact/*" element={<Page3 />} />
-        </Routes>
+        <div>
+          <Rotas />
+        </div>
       </div>
   );
 }
---
+++ src/Routes/Rotas.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Page1 from "../Pages/Pages1";
+import Page2 from "../Pages/Pages2";
+import Page3 from "../Pages/Pages3";
+
+export default function Rotas() {
+    return (
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="users/*" element={<Page2 />} />
+        <Route path="contact/*" element={<Page3 />} />
+      </Routes>
+    );
+}
